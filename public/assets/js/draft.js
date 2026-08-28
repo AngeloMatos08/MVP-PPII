5e419bed46eec157732be05459596c169946251d
AngeloMatos08 <angelomatosn@gmail.com>
2026-08-27 23:55:53 -0300

    feat: carrega os jogadores do json com função assíncrona

---
+++ public/assets/js/draft.js
@@ -0,0 +1,28 @@
+// Dev2-> AngeloMatos08: Aqui ficarão os jogadores "puxados do banco de dados"
+let listaDeJogadores = [];
+
+// Dev2-> AngeloMatos08: Função que puxa os jogadores do JSON
+
+// Dev2-> AngeloMatos08: Pq async? Pq a função fetch() é assíncrona, ou seja, 
+// ela não bloqueia a execução do código enquanto aguarda a resposta do servidor. 
+// Isso significa que o código pode continuar sendo executado enquanto a requisição está 
+// sendo processada, o que é importante para não travar a interface do usuário.
+async function puxarJogadores() {
+    try {
+        // Dev2-> AngeloMatos08: Indo até o arquivo JSON que contém os jogadores
+        const response = await fetch('assets/database/players.json');
+
+        listaDeJogadores = await response.json();
+
+        console.log('Jogadores carregados!', listaDeJogadores);
+        
+        // Dev2-> AngeloMatos08: Chamando a função que irá iniciar o draft
+        iniciarDraft();
+    } catch (error) {
+        console.error('Erro ao carregar jogadores:', error);
+    }
+}
+    function iniciarDraft() {
+        console.log('Iniciando o draft com os jogadores:' + listaDeJogadores.length + ' jogadores.');
+    }
+puxarJogadores();
