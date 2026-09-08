070187d666304f8a52bf9dc2734bf4a096bf5d04
Guimarães <pedrohenriqueguimaraesp@gmail.com>
2026-09-08 15:15:50 -0300

    Estrutura dos Times

---
+++ logic/teams/teams.js
@@ -0,0 +1,8 @@
+class team {
+    constructor (time_id, nome_time, ano) {
+        this.time_id = time_id;
+        this.nome_time = nome_time;
+        this.ano = ano;
+        this.jogadores = [];
+    }
+}
