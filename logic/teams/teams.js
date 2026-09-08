f2b4192f6276fc46f013d04173a0184d74fdfe77
Guimarães <pedrohenriqueguimaraesp@gmail.com>
2026-09-08 16:18:31 -0300

    Remoção Estruturas Desnecessárias

--- logic/teams/teams.js
+++
@@ -1,8 +0,0 @@
-class team {
-    constructor (time_id, nome_time, ano) {
-        this.time_id = time_id;
-        this.nome_time = nome_time;
-        this.ano = ano;
-        this.jogadores = [];
-    }
-}
