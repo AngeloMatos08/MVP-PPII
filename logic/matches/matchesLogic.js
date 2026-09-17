a90d479239e0fd0e85eb4f190f51261ee1dc71c2
Pedro <pedrohenriqueguimaraesp@gmail.com>
2026-09-17 10:49:51 -0300

    Implementação do cálculo de ataque das partidas

--- logic/matches/matchesLogic.js
+++ logic/matches/matchesLogic.js
@@ -1,1 +1,38 @@
 //Lógica responsável pela simulação das partidas
+
+// Função para obter Fatores de cada posição
+function obterFatoresFuncao(funcao) {
+    
+    switch (funcao) {
+
+    case "Duelista":
+        return { atkF: 1.40, defF: 1.00 };
+
+    case "Iniciador":
+        return { atkF: 1.25, defF: 1.15 };
+
+    case "Controlador":
+        return { atkF: 1.15, defF: 1.25 };
+
+    case "Sentinela":
+        return { atkF: 1.00, defF: 1.40 };
+
+    default:
+        return { atkF: 1.00, defF: 1.00 };
+    }
+}
+
+// Função para calcular o overall do Time
+
+function calcularAtaque(time) {
+    let soma = 0;
+
+    for (let jogador of time) {
+ 
+        const fatores = obterFatoresFuncao(jogador.funcao);
+
+        soma += jogador.overall * fatores.atkF;
+    }
+
+    return soma / 5;
+}
