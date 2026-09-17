ccfc18c2eee51ce9e14488d81de061772e7c4c6e
Guimarães <pedrohenriqueguimaraesp@gmail.com>
2026-09-17 16:22:19 -0300

    Área de teste para o código

--- logic/matches/matches.js
+++ logic/matches/matches.js
@@ -1,7 +1,11 @@
 class Match {
-    constructor(timeJogador, timeRival, mapa) {
-        this.timeJogador = timeJogador;
-        this.timeRival = timeRival;
-        this.mapa = mapa;
+    constructor(timeA, timeB) {
+        this.timeA = timeA;
+        this.timeB = timeB;
+
+        this.placarA = 0;
+        this.placarB = 0;
+
+        this.vencedor = null;
     }
 }
